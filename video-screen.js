edb4f056fa5aa8178252e95d9cbf3764ba0cedd9
Mascha van der Marel <43924750+MaschavanderMarel@users.noreply.github.com>
2020-06-13 18:09:07 +0200

    fix jump to breakdown function

--- video-screen.js
+++ video-screen.js
@@ -1,39 +1,52 @@
 
+function ended(){
+    document.location = 'underConstruction.html';
+}
+
+
 function jump(){
     var vid = document.getElementById("playingVideo");
-    document.location = 'breakdown.html#30';
     localStorage.currentTime = vid.currentTime;
-    // alert(localStorage.currentTime);
 
-    if (vid.currentTime > 30){
-        document.location = 'breakdown.html#30';
-    }
-    if (vid.currentTime > 60){
-        document.location = 'breakdown.html#60';
-     }
-     if (vid.currentTime > 90){
-        document.location = 'breakdown.html#90';
-     }
-      if (vid.currentTime > 120){
-        document.location = 'breakdown.html#120';
-    }
-    if (vid.currentTime > 150){
-        document.location = 'breakdown.html#150';
-    }
-    if (vid.currentTime > 180){
-        document.location = 'breakdown.html#180';
+    if (vid.currentTime > 240){
+        document.location = 'breakdown.html#240';
+        return;
     }
     if (vid.currentTime > 210){
         document.location = 'breakdown.html#210';
+        return;
     }
-    if (vid.currentTime > 240){
-        document.location = 'breakdown.html#240';
+    if (vid.currentTime > 180){
+        document.location = 'breakdown.html#180';
+        return;
+    }
+    if (vid.currentTime > 150){
+        document.location = 'breakdown.html#150';
+        return;
+    }
+    if (vid.currentTime > 120){
+        document.location = 'breakdown.html#120';
+        return;
+    }
+    if (vid.currentTime > 90){
+        document.location = 'breakdown.html#90';
+        return;
+     }
+     if (vid.currentTime > 60){
+        document.location = 'breakdown.html#60';
+        return;
+     }
+    if (vid.currentTime > 30){
+        document.location = 'breakdown.html#30';
+        return;
+    }
+    if (vid.currentTime >= 0){
+        document.location = 'breakdown.html';
     }
 }
 
 function setStartVideo(){
     var vid = document.getElementById("playingVideo");
-    // alert(localStorage.currentTime);
     vid.currentTime = localStorage.currentTime;
     localStorage.currentTime = 0;
 
